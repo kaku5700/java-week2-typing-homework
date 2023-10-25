2749522415cb3f2b0a18cd049a3059d36f9a7c82
kaku5700 <primekaku@gmail.com>
2023-10-25 17:25:44 +0100

    Java week 2 typing HW

---
+++ src/Programme_9_ConvertUpperToLowercase.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Write a program to convert the upper case to lower case.
+ */
+public class Programme_9_ConvertUpperToLowercase {
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the uppercase string : ");
+        String uppercase = scanner.nextLine();
+        Programme_9_ConvertUpperToLowercase t = new Programme_9_ConvertUpperToLowercase();
+        t.convertUppercaseToLowerCase(uppercase);
+        // closing the scanner object
+        scanner.close();
+    }
+    //Conversion of Uppercase to Lower case method
+    public void convertUppercaseToLowerCase(String text) {
+        System.out.println("The Lowercase value is = " + text.toLowerCase());
+    }
+}
